fc851525ba021cdaf3e5a989ed0fd1a46e150422
sanju29908 <sanjaymojeshk@gmail.com>
2025-03-29 11:17:48 +0530

    added code

---
+++ function.js
@@ -0,0 +1,87 @@
+
+function length (str){
+    return str.length
+}console.log(length("kalaga sanjay mojesh"));
+
+function charAt (str){
+    return str.charAt(5)
+}console.log(charAt("sanjay mojesh"));
+
+function indexOf (str){
+    return str.indexOf("y")
+}console.log(indexOf("sanjay mojesh"));
+
+function includes (str){
+    return str.includes("sanjay")
+}console.log(includes("sanjay mojesh"));
+
+function startswith (str){
+    return str.startsWith("sanjay")
+}console.log(startswith("sanjay mojesh"));
+
+
+function endswith (str){
+    return str.endsWith("h")
+}console.log(endswith("sanjaymojesh"));
+
+function replace (str){
+    return str.replace("sanjay","kalaga")
+}console.log(replace("sanjay mojesh"));
+
+function toUppercase (str){
+    return str.toUpperCase()
+}console.log(toUppercase("sanjay mojesh"));
+
+function splitted(str){
+    return str.split(" ")
+}console.log(splitted("sanjay mojesh"));
+
+function reverse(str){
+    return str.split("").reverse().join("")
+}console.log(reverse("sanjay mojesh"));
+
+function trim (str){
+    return str.trim()
+}console.log(trim("    sanjay moesh     "));
+
+
+function concat (str1,str2){
+    return str1.concat(str2)
+}console.log(concat("sanjay","mojesh"));
+
+function substring (str){
+    return str.substring(0,5)
+}console.log(substring("sanjay mojesh"));
+
+function padstart (str){
+    return str.padStart(20,"kalaga")
+}console.log(padstart("sanjay mojesh"));
+
+function padend (str){
+    return str.padEnd(16,"kalaga")
+}console.log(padend("sanjay mojesh"));
+
+function slice (str){
+    return str.slice(0,6)
+}console.log(slice("sanjay mojesh"));
+
+function string(str1,str2){
+    return parseInt(str1)+str2
+}console.log(string(12,32));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ string methods.js
@@ -0,0 +1,179 @@
+// var str ="java script"               
+// console.log(str.length);
+
+// var str ="java script"
+// console.log(str.charAt(2));
+
+// var str ="java script"
+// console.log(str.indexOf("s"));
+
+// var str ="java script"
+// console.log(str.includes("script"));
+
+// var str ="java script"
+// console.log(str.startsWith("j"));
+
+// var str ="java script"
+// console.log(str.endsWith("t"));
+
+// var str ="java script"
+// var newStr = str.replace("java","c++")
+// console.log(newStr);
+
+// var str ="java script"
+// console.log(str.toUpperCase());
+
+// var str ="JAVA SCRIPT"
+// console.log(str.toLowerCase());
+
+// var str = "        java   script       "
+// console.log(str.trim());
+
+
+// var str ="java script"
+// console.log(str.split());
+// console.log(str.split(""));
+// console.log(str.split(" "));
+
+// var str ="java script"
+// console.log(str.split("").reverse().join(""));
+
+// repeat
+
+// var str ="java script"
+// console.log(str.repeat(15));
+
+// concat
+
+// var str1 = "jaava"
+// var str2 = "script"
+// console.log(str1.concat(str2));
+
+// // substring
+
+// var str ="java script"
+// console.log(str.substring(0,6));
+
+// var  a= 10
+// var b = 20
+// console.log(`${a}and ${b}`);
+
+// var str ="java script"
+// console.log(str.padStart(15,"sanjay"));
+
+// var str ="java script"
+// console.log(str.padEnd(12,"java"));
+
+// var str ="java script"
+// console.log(str.slice(0,7));
+
+// var str = 10
+// console.log(parseInt(str+1));
+
+
+// var str = "sanjaymojesh"
+// console.log(str.length);
+
+// var str = "sanjaymojesh"
+// console.log(str.charAt(6));
+
+// var str = "sanjaymojesh"
+// console.log(str.includes("mojesh"));
+
+// var str = "sanjaymojesh"
+// console.log(str.indexOf("e"));
+
+// var str = "sanjaymojesh"
+// console.log(str.startsWith("sanjay"));
+
+// var str = "sanjaymojesh"
+// console.log(str.endsWith("mojesh"));
+
+
+// var str = "sanjaymojesh"
+// console.log(str.replace("mojesh","mercy"));
+
+// var str = "sanjaymojesh"
+// console.log(str.toUpperCase());
+
+// var str = "SANJAYMOJESH"
+// console.log(str.toLowerCase());
+
+// var str = "      sanjay  mojesh     "
+// console.log(str.trim());
+
+// var str = "sanjay  mojesh"
+// console.log(str.split());
+// console.log(str.split(""));
+// console.log(str.split(" "));
+
+// var str = "sanjaymojesh"
+// console.log(str.split("").reverse().join(""));
+
+// var str = "sanjaymojesh"
+// console.log(str.repeat(20));
+
+// var str1 = "sanjaymojesh"
+// var str2 = "kalaga"
+// console.log(str1.concat(str2));
+
+// var str = "sanjaymojesh"
+// console.log(str.substring(0,8));
+
+
+// var str = "sanjaymojesh"
+// console.log(str.padStart(19,"kalaga "));
+
+
+// var str = "sanjaymojesh"
+// console.log(str.padEnd(19," kalaga"));
+
+// var str = "sanjaymojesh"
+// console.log(str.slice(3,5));
+
+// var str = "10"
+// console.log(parseInt(str)-1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
