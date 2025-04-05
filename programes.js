1a2bb44737887b5a5207c10ae89adbf576115251
sanju29908 <sanjaymojeshk@gmail.com>
2025-04-05 12:40:57 +0530

    add all files

--- programes.js
+++ programes.js
@@ -12,10 +12,10 @@
     
 // }add(10,30)
 
-function add(a,b){
-    return(a%b)
-}
-console.log(add(10,20));
+// function add(a,b){
+//     return(a%b)
+// }
+// console.log(add(10,20));
 
 
 // //sqare root
@@ -43,53 +43,51 @@ console.log(add(10,20));
 // b=a
 // console.log(a,b);
 
-var random = Math.random()*1000
-console.log(random);
+// var random = Math.random()*1000
+// console.log(random);
 
-var myStr = random.toString()
-if(myStr.length==5){
-    console.log(myStr);
-}
+// var myStr = random.toString()
+// if(myStr.length==5){
+//     console.log(myStr);
+// }
 
-var myStr = random.toString()
-if(myStr.length==6){
-    console.log(myStr);
-}
-var celcius = 40
-var forienhight = celcius * 1.8 +43
-console.log(forienhight);
+// var myStr = random.toString()
+// if(myStr.length==6){
+//     console.log(myStr);
+// }
+// var celcius = 40
+// var forienhight = celcius * 1.8 +43
+// console.log(forienhight);
 
-var kilometres = 60
-var miles = kilometres +0.621371
-console.log(Math.floor(miles));
+// var kilometres = 60
+// var miles = kilometres +0.621371
+// console.log(Math.floor(miles));
 
-var kilometres = 60
-var miles = kilometres +0.621371
-console.log(miles);
+// var kilometres = 60
+// var miles = kilometres +0.621371
+// console.log(miles);
 
-var myDate = new Date()
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleTimeString());
+// var myDate = new Date()
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
 
 var a =10
 var b =30
 var c =40
-var d= b*b-4*a*c
+var d = b*b-4*a*c
 var root1;
 var root2;
 if(d>0){
     root1 = (-b+Math.sqrt(d))/2*a
     root2 = (-b-Math.sqrt(d))/2*a
     console.log(root1,root2);
-}else if(d==0){
+}else if(d ==0){
     root1 = root2 = -b/2*a
     console.log(root1,root2);
 }else{
     let realPart = (-b / (2 * a)).toFixed(2);
-    let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
+    let imagPart = (Math.sqrt(d)/(2*a)).toFixed(2);
     console.log(realPart,imagPart);
-    
-
 }
     
 
