20a5bb29404b0b32f1611c6305dd46b3d26028ce
sanju29908 <sanjaymojeshk@gmail.com>
2025-04-05 12:33:50 +0530

    add all files

---
+++ loop.js
@@ -0,0 +1,48 @@
+// for (var k = 0 ; k<=50 ; k++
+// )console.log(k);
+
+
+// for ( var l = 0 ; l<=100 ; l++)
+//     console.log(l);
+    
+
+// for (var h = 100 ; h<=10 ; h++)
+//     console.log(h);
+
+// var i = 3
+// var a = 10
+// while(i<=100){
+//     console.log(i);
+//     i++
+    
+// }
+//     var a = 4
+//     var y = 10
+//     while(a<=20){
+//         console.log(a);
+//         a++
+//     }
+
+
+// for (var i = 0 ; i<=100; i++){
+//     console.log(i);}
+
+// for (var k = 0 ; k<=50; k++){
+//     console.log(k);
+    
+// }    
+
+    
+var a = 10
+var i = 50
+
+do {jn
+    
+} while (condition);
+ 
+  
+  
+
+
+
+    
---
+++ statement.js
@@ -0,0 +1,67 @@
+// var a = 20
+// var b = 30
+// if (a>b){
+//     console.log("a is the bigg number");   
+// }else {console.log("b is the bigg number");
+// }
+
+// var a = 10 
+// var b = 30
+// var c = 500
+// var d = 60
+
+// if (a>b && a>c && a>d){
+//     console.log(a,"is the bigg number");
+// }else if(b>a && b>c && b>d){
+//     console.log(b,"is the bigg number");
+// }else if  (c>a && c>b && c>d){
+//     console.log(c, "is the bigg number");
+// }else {console.log("d is the bigg number");
+// }
+
+// var a = 20
+// var b = 40
+// var c = 50
+
+// var result =  a>b && a>c ? "a is the bigg number" : b>a && b>c ? " b is the bigg number" : "c is the bigg number"
+// console.log(result);
+
+// var time = "afternoon"
+// switch (time) {
+//     case "morning":
+//         console.log("good morning");
+//         break;
+//     case "afternoon":
+//         console.log("good afternoon");
+//         break;
+//     case "eavning":
+//         console.log("good eavning");
+//         break;
+
+//     default:("no value")
+//         break;
+// }
+
+// var day = "monday"
+// switch (day) {
+//     case "monday":
+//         console.log("happy monday");
+//         break;
+//     case "tuesday":
+//         console.log("happy tuesday");
+//         break;
+//     case "wednesday":
+//         console.log("happy wednesday");
+//         break;
+//      case "thurseday":
+//         console.log("haappy thurseday");
+    
+           
+                
+        
+        
+//         break;
+
+//     default:("no value")
+//         break;
+// }
