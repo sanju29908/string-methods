17b211e76f0ad16178ea91f4bf038ed252b3c194
sanju29908 <sanjaymojeshk@gmail.com>
2025-04-11 11:39:22 +0530

    add all files

---
+++ programe.js
@@ -0,0 +1,410 @@
+// // // print hello world
+//  console.log("HELLO WORLD");
+
+// // ADD TWO NUMBERS
+// var a = 10
+// var b = 20
+// var c = (a+b)
+// console.log(c);
+
+// function add(a,b){
+//     console.log(a+b);
+    
+// }add(10,30)
+
+// function add(a,b){
+//     return(a%b)
+// }
+// console.log(add(10,20));
+
+
+// //sqare root
+
+// var a = 55
+// var result =Math.sqrt(a)
+// console.log(result);
+
+// var a = 59
+// var result = Math.sqrt(a)
+// console.log(result);
+
+
+// //area of triangle
+
+// var areavalue = (4*10)/2;
+// console.log(areavalue);
+
+// // swap two variables
+
+// var a= 2
+// var b= 3
+// var c=a
+// a=b
+// b=c
+// console.log(a,b);
+
+// var random = Math.random()*1000
+// console.log(random);
+
+// var myStr = random.toString()
+// if(myStr.length==5){
+//     console.log(myStr);
+// }
+
+// var myStr = random.toString()
+// if(myStr.length==6){
+//     console.log(myStr);
+// }
+// var celcius = 40
+// var forienhight = celcius * 1.8 +43
+// console.log(forienhight);
+
+// var kilometres = 60
+// var miles = kilometres +0.621371
+// console.log(Math.floor(miles));
+
+// var kilometres = 60
+// var miles = kilometres +0.621371
+// console.log(miles);
+
+// var myDate = new Date()
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+// var a =10
+// var b =30
+// var c =40
+// var d = b*b-4*a*c
+// var root1;
+// var root2;
+// if(d>0){
+//     root1 = (-b+Math.sqrt(d))/2*a
+//     root2 = (-b-Math.sqrt(d))/2*a
+//     console.log(root1,root2);
+// }else if(d ==0){
+//     root1 = root2 = -b/2*a
+//     console.log(root1,root2);
+// }else{
+//     let realPart = (-b / (2 * a)).toFixed(2);
+//     let imagPart = (Math.sqrt(d)/(2*a)).toFixed(2);
+//     console.log(realPart,imagPart);
+// }
+    
+// var a ="hello world"
+// console.log("hello world");
+
+// var a =10
+// var b =20
+// var c =a+b
+// console.log(c);
+
+// function add(a,b){
+//     console.log(a+b);
+// }add (10,30)
+
+// var a =60
+// var result= Math.sqrt(a)
+// console.log(result);
+
+// var areavalue =(7*5)/2
+// console.log(areavalue);
+
+// var a = 20
+// var b = 30
+// var c = a
+// a=b
+// b=c
+// console.log(a,b);
+
+// var random = Math.floor(Math.random()*100)
+// console.log(random);
+
+// var a = 12
+// if(a%2 == 0){
+//     console.log(a, "is the even number");
+// }else{
+//     console.log(a, "is the odd number");
+    
+// }
+
+// var even = []
+// var odd =  []
+// for(var i = 1 ;  i<=100 ; i++){
+//     if(i%2 == 0){
+//         even.push(i)
+//     }else{
+//         odd.push(i)
+//     }
+// }console.log(even);
+// console.log(odd);
+
+// var even =[]
+// var odd = []
+// for(var k=1; k<=100 ; k++){
+
+//     if(k%2 == 0){
+//         even.push(k)
+        
+//     }else{
+//         odd.push(k)
+//     }
+// }console.log(even);
+// console.log(odd);
+
+// var a = 20
+// var b = 30
+// var c = 40
+
+// if(a>b && a>c){
+//     console.log(a, "is the bigg");
+// }else if(b>a && b>c){
+//     console.log(b, "is the bigg");
+// }else{
+//     console.log(c,"is the bigg");
+// }
+
+// var a = 20
+// var b = 60
+// var c = 80
+
+// var result= a>b && a>c ? "a is bigg": b>a && b>c ? "b is bigg" :"c is bigg"
+// console.log(result);
+
+// function bigg(a,b,c){
+//     return a>b && a>c ? "a is bigg": b>a && b>c ? "b is bigg" :"c is bigg"
+// }console.log(bigg(10,30,10));
+
+// var num= 5
+// var isPrime = true
+
+// for(var i=2 ; i<num ;  i++){
+//     if(num%i == 0){
+//         isPrime = false
+//     }
+// }console.log(isPrime);
+
+// var num = 17
+// var isPrime = true
+
+// for(var l=2 ; l<num ; l++){
+//     if(num%l == 0){
+//         isPrime =  false
+//     }
+// }console.log(isPrime);
+
+// function checkPrime(num){
+//     var isPrime = true
+//     for(var i=2 ; i<num ; i++){
+//         if(num%i == 0){
+//             isPrime = false
+//         }
+//     }console.log(isPrime);
+    
+// }checkPrime(7)
+
+
+// for(var i = 1; i<=100; i++){
+//     var isPrime = true
+//     for(var j=2 ;  j<i ; j++){
+//         if(i%j == 0){
+//             isPrime = false
+//         }
+//     }if(isPrime == true){
+//         arr.push(i)
+//         console.log(arr);
+//     }
+//         }
+
+// var num = 2
+// var factorial = 2
+// for(i = 1; i<=num; i++){
+//     factorial=factorial*i
+// }console.log(factorial);
+
+// var num = 6
+// var factorial = 2
+// for (i = 1; i<=num;  i++){
+//     factorial=factorial*i
+// }console.log(factorial);
+
+// var num = 4
+// var factorial = 2
+// for(i = 1;i<=num;i++){
+//     factorial=factorial*i
+// }console.log(factorial);
+
+// var num = 9
+// var isPrime = true
+
+// var num = 8
+// var isPrime=true
+
+// for(i=2;i<num;i++){
+//     if(num%i == 0){
+//         isPrime=false
+//         break
+//     }
+// }console.log(isPrime);
+
+// var even = []
+// var odd = []
+// for(i=1;i<=100;i++){
+//     if(i%2 == 0){
+//         even.push(i)
+//     }else{
+//         odd.push(i)
+//     }console.log(even);
+//     console.log(odd);
+
+// }
+    // var n = 5
+    // for(i=1;i<=10;i++){
+    //     console.log(`${n} * ${i}=${n*i}`);
+        
+    // }
+
+
+// var n1=0
+// var n2=1
+// var n3
+// for(var i=1;i<=10;i++){
+//     console.log(n1);
+//     n3=n1+n2
+//     n1=n2
+//     n2=n3
+    
+// }
+
+//armstrong number
+
+
+// var n = 153
+// var temp = n
+// var sum = 0
+// while (temp!=0) {
+//     var reminder = temp%10
+//     sum += Math.pow(reminder,3)
+//     temp = Math.floor(temp/10)
+    
+// }
+// if(sum ==n){
+//     console.log("armstrong")
+    
+// }
+
+// var n = 169
+// var temp = n
+// var sum = 0
+// while (temp!=0) {
+//     var remainder = temp%10
+//     sum+= Math.pow(remainder,3)
+    
+    
+//     temp = Math.floor(temp/10)
+    
+// }
+// if(sum == n){
+//     console.log("armstrong");
+    
+// }
+// var n = 16798
+// var temp = n
+// var str = n.toString().length
+
+// while (temp!=0) {
+//     var remainder = temp%10
+//     sum+= Math.pow(remainder,str)
+//     temp = Math.floor(temp/10)
+    
+// }
+// if(sum == n){
+//     console.log("armstrong");
+    
+// }
+
+// for(var n=1;n<=1000;n++){
+//     var temp = n
+//     var sum = 0
+//     var str = n.toString().length
+//     while (temp!=0) {
+//         var remainder = temp%10
+//         sum += Math.pow(remainder,str)
+//         temp = Math.floor(temp/10)
+        
+//     }if(n == sum){
+//         console.log(n);
+        
+//     }
+// }
+
+for(var i = 1; i<=1000;i++){
+    var temp = i
+    var sum =0
+    var str = i.toString().length
+    while (temp!=0) {
+        var remainder= temp%10
+        sum += Math.pow(remainder,str)
+        temp = Math.floor(temp/10)
+        
+    }if(i == sum){
+        console.log(i);
+        
+    }
+}
+
+var a = 10
+var b = 20
+var operater = "%"
+if(operater == "+"){
+    console.log(a+b);
+}else if(operater == "-"){
+    console.log(a-b);
+}else if(operater == "*"){
+    console.log(a*b);
+}else if(operater == "/"){
+    console.log(a/b);
+    
+}else if (operater == "%"){
+    console.log(a%b);
+    
+}
+
+
+
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
